fe5a306a3eed37beb5157fdf332164d86da0b296
MarviinFlores <marviinflores@criptext.com>
2023-01-19 18:55:07 -0500

    work section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import "./assets/js/script";
 import Header from './components/Header/Header';
-
+import Services from './components/Services/Services.jsx';
+import Work from './components/Work/Work.jsx'
 function App() {
   return (
       <div className="App">
         <Header />
+	<Services />
+	<Work />  
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./Navbar.css";
 import {Link} from "react-router-dom";
 import {FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar =() =>{
+	const [navToggler,setNavToggler]= useState(false);
+	const closeNavbar = () => setNavToggler(false); 
+
     return (
         <nav className='navbar'>
             <div className='container'>
                 <div className='navbar-content'>
                     <div className='brand-and-toggler flex flex-sb'> 
                     <Link to ="/" className='navbar-brand text-uppercase fw-7 text-white ls-2 fs-22'>GMTTService</Link>
-                     <button type='button' className='navbar-open-btn text-white'>
+                     <button type='button' className='navbar-open-btn text-white' onClick={()=>setNavToggler(!navToggler)}>
                             <FaBars size={30}/>
                     </button>
-			<div className ='navbar-collapse'>
-	    			<button type= "button" className = 'navbar-close-btn text-white'>
+			<div className ={navToggler ? "navbar-collapse show-navbar-collapse":"navbar-collapse"}>
+	    			<button type= "button" className = 'navbar-close-btn text-white' onClick = {closeNavbar} >
 	    			<FaTimes size ={30}/>
 	    			</button>
 	    			<ul className ='navbar-nav'>
