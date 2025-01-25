eac040d924baaab7ef74f646e31d493e04720e4d
MarviinFlores <marviinflores@criptext.com>
2025-01-24 22:37:39 -0500

    netlify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter, Router } from 'react-router-dom';
+import { HashRouter as router} from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
+//  <HashRouter>
+    <HashRouter>
       <Route path = "/" element = {<App />} ></Route>
-    </Routes>
-  </BrowserRouter>
+    </HashRouter>
+  //</HashRouter>
 );
 
