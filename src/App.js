00ffc98587ad8aa61703ac2d2b08704450719fc5
MarviinFlores <marviinflores@criptext.com>
2025-05-14 23:02:54 -0500

    whatsapp bottom

--- src/App.js
+++ src/App.js
@@ -9,19 +9,31 @@ import About from './components/About/About.jsx';
 import Testimonials from './components/Testimonials/Testimonials.jsx';
 import  Contact from './components/Contact/Contact.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import whatsappLogo from './assets/images/whatsappLogo.png'
 
 function App() {
   return (
+	<>
       <div className="App">
         <Header />
-	<Services />
-	<Work /> 
-	<About />  
-	<Testimonials />  
-	<Contact />  
-	<Footer />  
-    </div>
+     	<Services />
+    	<Work /> 
+	    <About />  
+	    <Testimonials />  
+    	<Contact />  
+    	<Footer /> 
+    <div class= "fixed-bottom right-100 p-3 " style = {{zIndex:"6",left: "initial"}}>
+		<a href="https://wa.me/+50765788940?text=Hello?" target='_blank'>
+		<img src={whatsappLogo} width ="60" alt="aaa"/>
+
+		</a>
+	</div>
+
+
+	</div>
+</>
   );
+  
 }
 
 export default App;
